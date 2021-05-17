8665037a560717fe643ceb2212534357813b9cf5
Natalya <natalya.zapolsk@gmail.com>
2021-05-17 22:20:35 +0300

    feat: tasks_2 complited

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,38 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    NSMutableArray *numbersArray = [[NSMutableArray alloc] init];
+    NSMutableArray *stringsArray = [[NSMutableArray alloc] init];
+    
+    for (NSArray *subArray in array) {
+        if (![subArray isKindOfClass:[NSArray class]]) {
+            return @[];
+        }
+        
+        if (subArray.count == 0) {
+            continue;
+        }
+        
+        if ([subArray.firstObject isKindOfClass:[NSNumber class]]) {
+            [numbersArray addObjectsFromArray:subArray];
+        }
+        
+        if ([subArray.firstObject isKindOfClass:[NSString class]]) {
+            [stringsArray addObjectsFromArray:subArray];
+        }
+    }
+    
+    if (stringsArray.count == 0) {
+        return [numbersArray sortedArrayUsingSelector:@selector(compare:)];
+    }
+    
+    if (numbersArray.count == 0) {
+        return [stringsArray sortedArrayUsingSelector:@selector(compare:)];
+    }
+    
+    NSSortDescriptor* sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:nil ascending:NO selector:@selector(localizedCompare:)];
+    return @[[numbersArray sortedArrayUsingSelector:@selector(compare:)], [stringsArray sortedArrayUsingDescriptors:@[sortDescriptor]]];
+
 }
 
 @end
